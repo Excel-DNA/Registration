3b1f29d42d1e89e1e930ee5c95ef3fd41c7fe07e
Govert van Drimmelen <govert@icon.co.za>
2016-12-24 13:42:44 +0200

    Fix F# registration in sample

--- Source/Samples/Registration.Sample.FSharp/ExampleAddIn.fs
+++ Source/Samples/Registration.Sample.FSharp/ExampleAddIn.fs
@@ -1,5 +1,6 @@
 ﻿namespace Registration.Samples.FSharp
 
+open System
 open ExcelDna.Integration
 open ExcelDna.Registration
 open ExcelDna.Registration.FSharp
@@ -7,8 +8,11 @@ open ExcelDna.Registration.FSharp
 type FsAsyncAddIn () =
     interface IExcelAddIn with
         member this.AutoOpen ()  = 
+            // The overload selection and delegate conversions performed by F# are not intuitive.
             let paramConvertConfig = ParameterConversionConfiguration()
-                                        .AddParameterConversion(FsParameterConversions.FsOptionalParameterConversion)
+                                        .AddParameterConversion( 
+                                            (fun (typ : Type) (reg : ExcelParameterRegistration) -> FsParameterConversions.FsOptionalParameterConversion typ reg),
+                                             null)
 
             ExcelRegistration.GetExcelFunctions ()
             |> fun fns -> ParameterConversionRegistration.ProcessParameterConversions (fns, paramConvertConfig)
