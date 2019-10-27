4da64223322d8f86861c48715abbeb8928a269ba
Caio Proiete <caio.proiete@gmail.com>
2019-10-27 01:58:08 -0300

    Update projects and NuGet packages to target Excel-DNA v1.0.0

--- Source/ExcelDna.Registration.FSharp/AssemblyInfo.fs
+++ Source/ExcelDna.Registration.FSharp/AssemblyInfo.fs
@@ -2,6 +2,6 @@
 
 open System.Reflection
 
-[<assembly: AssemblyVersion("0.34.*")>]
-[<assembly: AssemblyFileVersion("0.34.7.1")>]
+[<assembly: AssemblyVersion("0.35.0.0")>]
+[<assembly: AssemblyFileVersion("0.35.0.0")>]
 do()
--- Source/ExcelDna.Registration.VisualBasic/My Project/AssemblyInfo.vb
+++ Source/ExcelDna.Registration.VisualBasic/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.34.*")>
-<Assembly: AssemblyFileVersion("0.34.7.1")>
+<Assembly: AssemblyVersion("0.35.0.0")>
+<Assembly: AssemblyFileVersion("0.35.0.0")>
