e292e3e63be830918a4d8ef93281af24e17ce22d
govert <govert@dnakode.com>
2021-07-05 07:03:58 +0200

    Update to v1.5.0-rc1

--- NuGet/push.cmd
+++ NuGet/push.cmd
@@ -7,11 +7,11 @@ set outputPath=%basePath%\nupkg
 
 if not exist "%outputPath%" mkdir "%outputPath%"
 
-nuget.exe push "%outputPath%\ExcelDna.Registration.1.2.5-preview.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%outputPath%\ExcelDna.Registration.1.5.0-rc1.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
 
-nuget.exe push "%outputPath%\ExcelDna.Registration.FSharp.1.2.5-preview.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%outputPath%\ExcelDna.Registration.FSharp.1.5.0-rc1.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
 
-nuget.exe push "%outputPath%\ExcelDna.Registration.VisualBasic.1.2.5-preview.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%outputPath%\ExcelDna.Registration.VisualBasic.1.5.0-rc1.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
