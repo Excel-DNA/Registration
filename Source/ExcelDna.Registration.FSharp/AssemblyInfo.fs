a41a8747d77ac23416a9a77c9674c35a06310382
Govert van Drimmelen <govert@dnakode.com>
2021-12-22 18:13:33 +0200

    Release v1.5.1

--- Source/ExcelDna.Registration.FSharp/AssemblyInfo.fs
+++ Source/ExcelDna.Registration.FSharp/AssemblyInfo.fs
@@ -3,6 +3,6 @@
 open System.Reflection
 
 [<assembly: AssemblyVersion("1.1.0.0")>]
-[<assembly: AssemblyFileVersion("1.2.3.0")>]
-[<assembly: AssemblyInformationalVersion("1.2.3")>]
+[<assembly: AssemblyFileVersion("1.5.1.0")>]
+[<assembly: AssemblyInformationalVersion("1.5.1")>]
 do()
