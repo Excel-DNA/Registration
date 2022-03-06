1741069747cab32be7e446a15ad7593b77711df4
Govert van Drimmelen <govert@dnakode.com>
2022-03-06 11:30:48 +0200

    Update to v1.6.0-preview3

--- Source/ExcelDna.Registration.FSharp/AssemblyInfo.fs
+++
@@ -1,8 +0,0 @@
-﻿module AssemblyInfo
-
-open System.Reflection
-
-[<assembly: AssemblyVersion("1.1.0.0")>]
-[<assembly: AssemblyFileVersion("1.5.1.0")>]
-[<assembly: AssemblyInformationalVersion("1.5.1")>]
-do()
