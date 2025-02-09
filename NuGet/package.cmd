a414c901df2801abae3839bfff91b4be291e72d6
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-02-09 14:22:46 +0500

    Removed the code from ExcelDna.Registration.FSharp package.

--- NuGet/package.cmd
+++ NuGet/package.cmd
@@ -14,7 +14,7 @@ echo on
 nuget.exe pack "%basePath%\ExcelDna.Registration\ExcelDna.Registration.nuspec" -BasePath "%basePath%\ExcelDna.Registration" -OutputDirectory "%outputPath%" -Verbosity detailed -NonInteractive -Prop ExcelDnaVersion="%ExcelDnaVersion%"
 @if errorlevel 1 goto end
 
-nuget.exe pack "%basePath%\ExcelDna.Registration.FSharp\ExcelDna.Registration.FSharp.nuspec" -BasePath "%basePath%\ExcelDna.Registration.FSharp" -OutputDirectory "%outputPath%" -Verbosity detailed -NonInteractive -Prop ExcelDnaVersion="%ExcelDnaVersion%" -Symbols -SymbolPackageFormat snupkg
+nuget.exe pack "%basePath%\ExcelDna.Registration.FSharp\ExcelDna.Registration.FSharp.nuspec" -BasePath "%basePath%\ExcelDna.Registration.FSharp" -OutputDirectory "%outputPath%" -Verbosity detailed -NonInteractive -Prop ExcelDnaVersion="%ExcelDnaVersion%"
 @if errorlevel 1 goto end
 
 nuget.exe pack "%basePath%\ExcelDna.Registration.VisualBasic\ExcelDna.Registration.VisualBasic.nuspec" -BasePath "%basePath%\ExcelDna.Registration.VisualBasic" -OutputDirectory "%outputPath%" -Verbosity detailed -NonInteractive -Prop ExcelDnaVersion="%ExcelDnaVersion%" -Symbols -SymbolPackageFormat snupkg
