3a174bd109a6b38f6ebc9b016dc190346d7f202e
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2025-02-09 11:58:36 +0500

    Removed the code from ExcelDna.Registration package.

--- NuGet/package.cmd
+++ NuGet/package.cmd
@@ -11,7 +11,7 @@ if exist "%outputPath%\*.nupkg" del "%outputPath%\*.nupkg"
 if not exist "%outputPath%" mkdir "%outputPath%"
 
 echo on
-nuget.exe pack "%basePath%\ExcelDna.Registration\ExcelDna.Registration.nuspec" -BasePath "%basePath%\ExcelDna.Registration" -OutputDirectory "%outputPath%" -Verbosity detailed -NonInteractive -Prop ExcelDnaVersion="%ExcelDnaVersion%" -Symbols -SymbolPackageFormat snupkg
+nuget.exe pack "%basePath%\ExcelDna.Registration\ExcelDna.Registration.nuspec" -BasePath "%basePath%\ExcelDna.Registration" -OutputDirectory "%outputPath%" -Verbosity detailed -NonInteractive -Prop ExcelDnaVersion="%ExcelDnaVersion%"
 @if errorlevel 1 goto end
 
 nuget.exe pack "%basePath%\ExcelDna.Registration.FSharp\ExcelDna.Registration.FSharp.nuspec" -BasePath "%basePath%\ExcelDna.Registration.FSharp" -OutputDirectory "%outputPath%" -Verbosity detailed -NonInteractive -Prop ExcelDnaVersion="%ExcelDnaVersion%" -Symbols -SymbolPackageFormat snupkg
